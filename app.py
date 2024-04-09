df854dafa8ced7144c3c32091f442577f22b21f4
daithyr <159961434+daithyr@users.noreply.github.com>
2024-04-08 23:07:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_boston
+
 
 # Load Boston housing data
 @st.cache
